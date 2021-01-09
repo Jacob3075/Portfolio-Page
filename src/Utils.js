f1b150345aa93b6032e8b08ccddf55e421eee016
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-01-09 10:53:10 +0530

    Added useWindowSize hook to Utils.js.

--- src/Utils.js
+++ src/Utils.js
@@ -1,6 +1,21 @@
 import { ReactComponent as JavaIcon } from "./images/JavaIcon.svg";
 import { ReactComponent as JavaScriptIcon } from "./images/JavaScriptIcon.svg";
 
+import React, { useLayoutEffect, useState } from 'react';
+
+export const useWindowSize = () => {
+	const [size, setSize] = useState([0, 0]);
+	useLayoutEffect(() => {
+		function updateSize() {
+			setSize([window.innerWidth, window.innerHeight]);
+		}
+		window.addEventListener('resize', updateSize);
+		updateSize();
+		return () => window.removeEventListener('resize', updateSize);
+	}, []);
+	return size;
+};
+
 export const getIconByLanguage = (language) => {
 	language = language.toLowerCase();
 	switch (language) {
